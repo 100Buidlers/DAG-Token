e2c9fb5acfec7ffd57f2be6cbd00d3d7d93560c6
Somesh <somesh.08.96@gmail.com>
2022-09-15 00:56:52 +0530

    height 2 tree test case

--- contracts/SuperSolid.sol
+++ contracts/SuperSolid.sol
@@ -56,12 +56,13 @@ contract SuperSolid is Context {
         bool ltreeRes;
         uint256 startLtree = 4;
         uint256 ltreeBytesUsed;
+        // uint256 newLevel = level + 1;
         nextidentifier = bytes4(cBytes[startLtree:]);
         if (
             (nextidentifier == AND_IDENTIFIER) ||
             (nextidentifier == OR_IDENTIFIER) ||
             (nextidentifier == NAND_IDENTIFIER)
-        ) {
+        ) {            
             (ltreeRes, ltreeBytesUsed) = _evaluate_op(
                 cBytes[startLtree:],
                 iBytes,
@@ -71,9 +72,10 @@ contract SuperSolid is Context {
             (ltreeRes, ltreeBytesUsed) = _evaluate_adapter(cBytes[startLtree:], account);
         }
 
+
         // evaluation rtree
-        bool rtreeRes;
         uint256 startRtreeRes = startLtree + ltreeBytesUsed;
+        bool rtreeRes;
         uint256 rtreeBytesUsed;
         nextidentifier = bytes4(cBytes[startRtreeRes:]);
         if (
@@ -93,7 +95,7 @@ contract SuperSolid is Context {
         }
         return (
             _logicalOperatorMap[opIdentifier](ltreeRes, rtreeRes),
-            ltreeBytesUsed + rtreeBytesUsed
+            4 + ltreeBytesUsed + rtreeBytesUsed
         );
     }
 
@@ -113,50 +115,6 @@ contract SuperSolid is Context {
         return (IAdapter(adapterAddress).evaluate(aBytes, account), end);
     }
 
-    // function mergeBytes(bytes memory a, bytes20 b)
-    //     public
-    //     pure
-    //     returns (bytes memory c)
-    // {
-    //     // Store the length of the first array
-    //     uint256 alen = a.length;
-    //     // Store the length of BOTH arrays
-    //     uint256 totallen = alen + b.length;
-    //     // Count the loops required for array a (sets of 32 bytes)
-    //     uint256 loopsa = (a.length + 31) / 32;
-    //     // Count the loops required for array b (sets of 32 bytes)
-    //     uint256 loopsb = (b.length + 31) / 32;
-    //     assembly {
-    //         let m := mload(0x40)
-    //         // Load the length of both arrays to the head of the new bytes array
-    //         mstore(m, totallen)
-    //         // Add the contents of a to the array
-    //         for {
-    //             let i := 0
-    //         } lt(i, loopsa) {
-    //             i := add(1, i)
-    //         } {
-    //             mstore(
-    //                 add(m, mul(32, add(1, i))),
-    //                 mload(add(a, mul(32, add(1, i))))
-    //             )
-    //         }
-    //         // Add the contents of b to the array
-    //         for {
-    //             let i := 0
-    //         } lt(i, loopsb) {
-    //             i := add(1, i)
-    //         } {
-    //             mstore(
-    //                 add(m, add(mul(32, add(1, i)), alen)),
-    //                 mload(add(b, mul(32, add(1, i))))
-    //             )
-    //         }
-    //         mstore(0x40, add(m, add(32, totallen)))
-    //         c := m
-    //     }
-    // }
-
     // logical operators ====================================
     function and(bool a, bool b) private pure returns (bool) {
         return a && b;
@@ -168,30 +126,5 @@ contract SuperSolid is Context {
 
     function nand(bool a, bool b) private pure returns (bool) {
         return !(a && b);
-    }
-
-    function _msgSender()
-        internal
-        view
-        override(Context)
-        returns (address sender)
-    {
-        if (isTrustedForwarder(msg.sender)) {
-            // The assembly code is more direct than the Solidity version using `abi.decode`.
-            assembly {
-                sender := shr(96, calldataload(sub(calldatasize(), 20)))
-            }
-        } else {
-            return super._msgSender();
-        }
-    }
-
-    function isTrustedForwarder(address forwarder)
-        public
-        view
-        virtual
-        returns (bool)
-    {
-        return forwarder == _trustedForwarder;
     }
 }
--- test/ActivityToken.js
+++ test/ActivityToken.js
@@ -31,12 +31,18 @@ describe("ActivityToken", function () {
       // deploy the test erc721 contract
       let testERC721a;
       let testERC721b;
+      let testERC721c;
       [testERC721a] = await deployTestERC721();
       [testERC721b] = await deployTestERC721();
+      [testERC721c] = await deployTestERC721();
       await expect(
         testERC721a.mint()).to.emit(testERC721a, "Transfer").withArgs(nullAddress, owner.address, 0);
+      // await expect(
+      //   testERC721b.mint()).to.emit(testERC721b, "Transfer").withArgs(nullAddress, owner.address, 0);
       await expect(
-        testERC721b.mint()).to.emit(testERC721b, "Transfer").withArgs(nullAddress, owner.address, 0);
+        testERC721c.mint()).to.emit(testERC721c, "Transfer").withArgs(nullAddress, owner.address, 0);
+      // await expect(
+      //   testERC721c.mint()).to.emit(testERC721c, "Transfer").withArgs(nullAddress, owner.address, 1);
 
       let node1 = new TreeNode(
         [erc721BalanceThreshold.address.slice(2), testERC721a.address.slice(2), getBytes4HexKeccack("gt"), defaultAbiCoder.encode(["uint256"], [0]).slice(2)],
@@ -53,15 +59,37 @@ describe("ActivityToken", function () {
       console.log(node2.data);
 
       let node3 = new TreeNode(
+        [erc721BalanceThreshold.address.slice(2), testERC721c.address.slice(2), getBytes4HexKeccack("gt"), defaultAbiCoder.encode(["uint256"], [0]).slice(2)],
+        false,
+        "ERC721BalanceThreshold"
+      )
+      console.log(node3.data);
+
+      let op1 = new TreeNode(
         [getBytes4HexKeccack("and")],
         true,
         "and"
       )
-      console.log(node3.data);
+      console.log(op1.data);
 
-      node3.set_left(node1)
-      node3.set_right(node2)
-      bytes = encodeConditions(node3);
+      let op2 = new TreeNode(
+        [getBytes4HexKeccack("or")],
+        true,
+        "or"
+      )
+      console.log(op2.data);
+
+      op2.set_left(node1)
+      op2.set_right(node2)
+
+      op1.set_right(op2);
+      op1.set_left(node3);
+
+      // op1.set_left(op2);
+      // op1.set_right(node3);
+
+      bytes = encodeConditions(op1);
+      console.log(bytes);
 
       expect(await activitytoken.superSolidAddress()).to.equal(superSolid.address);
       await expect(
